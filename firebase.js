c0916452e46f4aaa00cb54bb807e26e5c2b6ef3d
Eddy-scion <justiceogbevoen@gmail.com>
2022-10-12 22:19:40 +0200

    Connect react app to firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK38oNCTOloHPPhk8_vBHo325FIdJprsU",
+  authDomain: "whatsapp-clone-b62fc.firebaseapp.com",
+  projectId: "whatsapp-clone-b62fc",
+  storageBucket: "whatsapp-clone-b62fc.appspot.com",
+  messagingSenderId: "1027095792012",
+  appId: "1:1027095792012:web:d9ab84b2d248bd7c08aea9",
+};
+
+const app = initializeApp(firebaseConfig);
